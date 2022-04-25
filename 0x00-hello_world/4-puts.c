2a0056d6a2e9d9403e0d497c0a1ea3ad0f502175
kuleniabera <kuleni@example.com>
2022-04-25 12:39:41 -0700

    This is the second message for 4-puts stating the change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /*
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * /
-int main(void) 
+ */
+int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzel");
-                return (0);
+	puts("\"Programming is like building a multilingual puzzel");
+	        return(0);
 }
